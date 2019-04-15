f43bfabcaac589d36a19eab770a5fd7f96edcc1d
snowaver <1123009419@qq.com>
2019-04-15 18:19:56 +0800

    1/expand functions in json utils.

--- mashroom-util/src/main/java/cc/mashroom/util/JsonUtils.java
+++ mashroom-util/src/main/java/cc/mashroom/util/JsonUtils.java
@@ -20,30 +20,43 @@ import  java.util.Map;
 import  java.util.TimeZone;
 
 import  com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import  com.fasterxml.jackson.databind.DeserializationFeature;
 import  com.fasterxml.jackson.databind.ObjectMapper;
 import  com.fasterxml.jackson.databind.module.SimpleModule;
 
 public  class  JsonUtils
 {
-	public  final  static  ObjectMapper  mapper    = new  ObjectMapper();
+	public  final  static  ObjectMapper  mapper      = new  ObjectMapper();
 	
 	static
 	{
 		SimpleDateFormat  greenwichDateFormater = new  SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'" );
 		
-		greenwichDateFormater.setTimeZone( TimeZone.getTimeZone("UTC") );
+		greenwichDateFormater.setTimeZone( TimeZone.getTimeZone( "UTC" ) );
 		
 		mapper.setDateFormat(greenwichDateFormater).configure( DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES , false );
 		
 		mapper.registerModule( new  SimpleModule().addDeserializer(cc.mashroom.util.collection.map.Map.class,new  MapDeserializer()) );
 	}
 	
-	public  static  Map<String,?>  fromJson(   String  json )
+	public  static  <T>  T  fromJson( String  jsonString,TypeReference<T>  typeReference )
 	{
 		try
 		{
-			return  mapper.readValue( json,Map.class );
+			return  mapper.readValue( jsonString,typeReference );
+		}
+		catch( Throwable  e)
+		{
+			throw  new  IllegalStateException( String.format("MASHROOM-UTIL:  ** JSONUTILS **  error  parsing  json  to  %s  instance.",Map.class.getName()),e );
+		}
+	}
+	
+	public  static  <T>  T  fromJson( String  jsonString,Class<T>  clazz )
+	{
+		try
+		{
+			return  mapper.readValue(jsonString,clazz);
 		}
 		catch( Throwable  e)
 		{
