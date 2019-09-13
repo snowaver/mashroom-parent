3d8eea7a510bf703b3d0ddd7961cb7a20013e598
snowaver <1123009419@qq.com>
2019-09-14 01:13:23 +0800

    1/redesign the router module.

---
+++ mashroom-router/src/main/java/cc/mashroom/router/DefaultServiceListRequestStrategy.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2019 snowaver.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cc.mashroom.router;
+
+import  java.net.URL;
+import  java.util.Collection;
+import  java.util.LinkedList;
+import  java.util.List;
+import  java.util.concurrent.CountDownLatch;
+import  java.util.concurrent.LinkedBlockingQueue;
+import  java.util.concurrent.ThreadPoolExecutor;
+import  java.util.concurrent.TimeUnit;
+
+import  javax.annotation.Nonnull;
+import  javax.net.ssl.SSLSocketFactory;
+
+import  cc.mashroom.util.JsonUtils;
+import  cc.mashroom.util.NoopHostnameVerifier;
+import  cc.mashroom.util.NoopX509TrustManager;
+import  lombok.AccessLevel;
+import  lombok.Setter;
+import  lombok.experimental.Accessors;
+import  okhttp3.OkHttpClient;
+import  okhttp3.Request;
+import  okhttp3.Response;
+
+public  class  DefaultServiceListRequestStrategy  implements  ServiceListRequestStrategy
+{
+	public  DefaultServiceListRequestStrategy( @Nonnull  Collection<String>  urls,@Nonnull  SSLSocketFactory  sslSocketFactory,long  timeout,@Nonnull  TimeUnit  timeunit )
+	{
+		this.setUrls(urls).setClient( new  OkHttpClient.Builder().hostnameVerifier(new  NoopHostnameVerifier()).sslSocketFactory(sslSocketFactory,new  NoopX509TrustManager()).connectTimeout(5,TimeUnit.SECONDS).writeTimeout(5,TimeUnit.SECONDS).readTimeout(timeout,timeunit).build() );
+	}
+	
+	@Setter( value= AccessLevel.PROTECTED )
+	@Accessors( chain= true )
+	private  OkHttpClient  client;
+	@Setter( value= AccessLevel.PROTECTED )
+	@Accessors( chain= true )
+	private  Collection<String>   urls;
+		
+	public  List<Service>  request()
+	{
+		return  invokeAny( true,urls );
+	}
+	
+	private  List<Service>  invokeAny( boolean  isErrorPermissible    ,Collection<String>  urls )
+	{
+		final  List<Service>  balancingProxies = new  LinkedList<Service>();
+		
+		if( urls.isEmpty() )
+		{
+			return    balancingProxies;
+		}
+		
+		ThreadPoolExecutor  executor = new  ThreadPoolExecutor( urls.size(),urls.size(),60,TimeUnit.SECONDS,new  LinkedBlockingQueue<Runnable>() );
+		
+		final  CountDownLatch  cdlatcher = new  CountDownLatch( urls.size() );
+		
+		for(final  String  url : urls )
+		{
+			executor.execute
+			(
+				new  Runnable()
+				{
+					public  void  run()
+					{
+						try( Response  response = client.newCall(new  Request.Builder().url(url).build()).execute() )
+						{
+							synchronized( DefaultServiceListRequestStrategy.class )
+							{
+								if( balancingProxies.isEmpty() )
+								{
+									if( response.code() == 200 )
+									{
+										String  connectingHost  = new  URL(url).getHost();
+										
+										for( Service  balancingProxy : (List<Service>)  JsonUtils.mapper.readValue(response.body().string(),JsonUtils.mapper.getTypeFactory().constructParametricType(List.class,Service.class)) )
+										{
+											if( connectingHost.equals(balancingProxy.getHost().trim()) )
+											{
+												balancingProxies.add(    balancingProxy );
+											}
+										}
+									}
+								}
+							}
+						}
+						catch(    Throwable  ioe )
+						{
+							ioe.printStackTrace();
+						}
+						finally
+						{
+							cdlatcher.countDown();
+						}
+					}
+				}
+			);
+		}
+		
+		try
+		{
+			cdlatcher.await( 5,TimeUnit.SECONDS );
+		}
+		catch( InterruptedException  e )
+		{
+			
+		}
+		
+		executor.shutdownNow();
+		
+		return    balancingProxies;
+		/*
+		if( balancingProxies.isEmpty() )
+		{
+			throw  new  IllegalStateException( "SQUIRREL-ROUTER:  ** DEFAULT  BALANCING  PROXY  FACTORY **  no  proxy  is  available" );
+		}
+		*/
+	}
+}
---
+++ mashroom-router/src/main/java/cc/mashroom/router/ServiceRouteManager.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019 snowaver.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cc.mashroom.router;
+
+import  java.util.List;
+
+import  org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
+
+import  cc.mashroom.util.collection.map.HashMap;
+import  cc.mashroom.util.collection.map.Map;
+import  lombok.AccessLevel;
+import  lombok.NoArgsConstructor;
+
+@NoArgsConstructor( access  = AccessLevel.PRIVATE )
+
+public  class  ServiceRouteManager
+{
+	public  final  static  ServiceRouteManager  INSTANCE = new  ServiceRouteManager();
+	
+	private  ServiceListRequestStrategy   strategy;
+	
+	private  ArrayListValuedHashMap<Schema,Service>  services = new  ArrayListValuedHashMap<Schema,Service>();
+	
+	private  Map<Schema , Service>    currents= new  HashMap<Schema,Service>();
+	
+	public   Service  tryNext(     Schema  schema )
+	{
+		Service  currentService = this.currents.get( schema );
+		
+		List<Service>  pendingServices = this.services.get( schema );
+		
+		if( pendingServices.isEmpty() )
+		{
+			return    null;
+		}
+		else
+		{
+			return  pendingServices.get( currentService== null || pendingServices.indexOf(currentService) == pendingServices.size()-1 ? 0 : pendingServices.indexOf(currentService)+1 );
+		}
+	}
+	
+	public  Service  current(      Schema  schema )
+	{
+		return  currents.get( schema );
+	}
+	
+	public  void  request()
+	{
+		for( Service  service: strategy.request() )
+		{
+			this.services.put( Schema.valueOf(service.getSchema().toString().toLowerCase())   , service );
+		}
+	}
+}
