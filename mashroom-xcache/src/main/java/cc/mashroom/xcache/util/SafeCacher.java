adc494862103f0023bb28f9201509a504696f83a
snowaver <1123009419@qq.com>
2019-10-14 15:16:32 +0800

    1/add safe cacher.

---
+++ mashroom-xcache/src/main/java/cc/mashroom/xcache/util/SafeCacher.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 snowaver.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cc.mashroom.xcache.util;
+
+import  java.util.concurrent.Callable;
+import  java.util.concurrent.TimeUnit;
+import  java.util.concurrent.locks.Lock;
+
+import  cc.mashroom.xcache.XKeyValueCache;
+
+public  class  SafeCacher
+{
+	/**
+	 *  acquire  the  lock,  call  the  callable  and  cache  the  value  if  not  cached,  returns  the  cached  value  and  unlock  the  locker  finally.
+	 */
+	public  static  <K,V>  V  cache( Lock  locker,long  acquireLockTimeout,TimeUnit  acquireLockTimeoutTimeUnit,XKeyValueCache<K,V>  cache,K  key,Callable<V>  callable )
+	{
+		V  value     = null;
+		
+		try
+		{
+			if( !locker.tryLock(acquireLockTimeout ,acquireLockTimeoutTimeUnit) )
+			{
+				throw  new  IllegalStateException( "SQUIRREL-XCACHE:  ** SAFE  CACHER **  the  lock  is  not  acquired  before  the  waiting  time  (2  seconds)  elapsed,  give  up." );
+			}
+			
+			if( (value = cache.get(key)) == null )       value = callable.call();
+		}
+		catch(Throwable  e )
+		{
+			throw  new  RuntimeException( e );
+		}
+		finally
+		{
+			locker.unlock();
+		}
+		
+		return  value;
+	}
+}
